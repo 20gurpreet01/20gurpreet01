612865c5746e411d450c60479a8af128f7857ad5
GurpreetSingh2001 <73258281+GurpreetSingh2001@users.noreply.github.com>
2021-12-20 02:05:09 +0530

    init1

---
+++ pract_pages/os1_prac_app.js
@@ -0,0 +1,66 @@
+$(document).ready(function() {
+    $("#q1a, #q2a, #q3a, #q4a, #q5a, #q6a, #q7a, #q8a, #q9a, #q10a, #q11a, #q12a, #q13a, #q14a, #q15a").hide();
+
+    $("#q1").click(function() {
+        $("#q1a").slideToggle("fast");
+        $("#dd1").toggleClass("rotate rotatetransition");
+    });
+
+    $("#q2").click(function() {
+        $("#q2a").slideToggle("fast");
+        $("#dd2").toggleClass("rotate rotatetransition");
+    });
+
+    $("#q3").click(function() {
+        $("#q3a").slideToggle("fast");
+        $("#dd3").toggleClass("rotate rotatetransition");
+    });
+    $("#q4").click(function() {
+        $("#q4a").slideToggle("fast");
+        $("#dd4").toggleClass("rotate rotatetransition");
+    });
+    $("#q5").click(function() {
+        $("#q5a").slideToggle("fast");
+        $("#dd5").toggleClass("rotate rotatetransition");
+    });
+    $("#q6").click(function() {
+        $("#q6a").slideToggle("fast");
+        $("#dd6").toggleClass("rotate rotatetransition");
+    });
+    $("#q7").click(function() {
+        $("#q7a").slideToggle("fast");
+        $("#dd7").toggleClass("rotate rotatetransition");
+    });
+    $("#q8").click(function() {
+        $("#q8a").slideToggle("fast");
+        $("#dd8").toggleClass("rotate rotatetransition");
+    });
+    $("#q9").click(function() {
+        $("#q9a").slideToggle("fast");
+        $("#dd9").toggleClass("rotate rotatetransition");
+    });
+    $("#q10").click(function() {
+        $("#q10a").slideToggle("fast");
+        $("#dd10").toggleClass("rotate rotatetransition");
+    });
+    $("#q11").click(function() {
+        $("#q11a").slideToggle("fast");
+        $("#dd11").toggleClass("rotate rotatetransition");
+    });
+    $("#q12").click(function() {
+        $("#q12a").slideToggle("fast");
+        $("#dd12").toggleClass("rotate rotatetransition");
+    });
+    $("#q13").click(function() {
+        $("#q13a").slideToggle("fast");
+        $("#dd13").toggleClass("rotate rotatetransition");
+    });
+    $("#q14").click(function() {
+        $("#q14a").slideToggle("fast");
+        $("#dd14").toggleClass("rotate rotatetransition");
+    });
+    $("#q15").click(function() {
+        $("#q15a").slideToggle("fast");
+        $("#dd15").toggleClass("rotate rotatetransition");
+    });
+});
